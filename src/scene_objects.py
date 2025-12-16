1931e908563e32d8609ee3c86394be5b8ca1d188
shplok <sawyerbowerman2@gmail.com>
2025-12-16 08:37:50 -0500

    Add the Gather bbox function

--- src/scene_objects.py
+++ src/scene_objects.py
@@ -1,5 +1,9 @@
 import sionna.rt as rt
 from sionna.rt import Camera, Transmitter
+import os
+import numpy as np
+import trimesh
+from collections import defaultdict
 
 def create_scene_objects(scene):   
     # Set up antenna arrays
@@ -31,3 +35,54 @@ def create_scene_objects(scene):
     map_center = [(x_min + x_max) / 2, (y_min + y_max) / 2, z_height]
 
     return bounds, map_center, (map_width, map_height), cell_size
+
+def gather_bboxes(MESH_DIR):
+
+    raw_bbox_data = []    
+    files = [f for f in os.listdir(MESH_DIR) if f.endswith(".ply")]
+    
+    for i, file in enumerate(files):
+        mesh_path = os.path.join(MESH_DIR, file)
+        try:
+            # force='mesh' prevents trimesh from trying to load as a scene
+            mesh = trimesh.load(mesh_path, force='mesh')
+            
+            # Trimesh bounds returns [[min_x, min_y, min_z], [max_x, max_y, max_z]]
+            bbox_min = mesh.bounds[0]
+            bbox_max = mesh.bounds[1]
+            
+            raw_bbox_data.append({
+                "file": file,
+                "bbox_min": bbox_min,
+                "bbox_max": bbox_max
+            })
+            
+        except Exception as e:
+            print(f"Error loading {file}: {e}")
+            continue
+
+    building_groups = defaultdict(list)
+    for b in raw_bbox_data:
+        # Strip suffixes
+        base_name = b['file'].replace('-itu_concrete.ply', '').replace('-itu_metal.ply', '')
+        building_groups[base_name].append(b)
+
+    merged_bboxes = []
+    
+    for name, meshes in building_groups.items():
+        # Stack all mins and maxs for this building
+        mins = np.array([m['bbox_min'] for m in meshes])
+        maxs = np.array([m['bbox_max'] for m in meshes])
+        
+        # Find the extreme corners
+        final_min = mins.min(axis=0)
+        final_max = maxs.max(axis=0)
+        
+        merged_bboxes.append({
+            "name": name,
+            "min": final_min,
+            "max": final_max
+        })
+
+    print(f"Processed {len(merged_bboxes)} unique structures.")
+    return merged_bboxes
